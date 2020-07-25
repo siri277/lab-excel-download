d2105ab16b4049917a002ee2bc06f88a21830bdb
siri <dsirichandana1703@gmail.com>
2020-07-25 14:02:51 +0530

    FSJ0620D103

--- src/service/ExcelGenerator.java
+++ src/service/ExcelGenerator.java
@@ -1,13 +1,15 @@
 package service;
-/*
+
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.List;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import model.Prograd;
 
@@ -19,20 +21,34 @@ import model.Prograd;
 public class ExcelGenerator {
 	
 	FileOutputStream out;
-	public HSSFWorkbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
+	String filename = "F:\\excel.xlsx";
+	int rowcount = 0;
+	FileWriter fw;
+	public XSSFWorkbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
 		try {
-
-
+			XSSFWorkbook xwb = new XSSFWorkbook();
+			XSSFSheet sheet=xwb.createSheet("Feedback");
+			for(Prograd listitem:list) {
+				XSSFRow row = sheet.createRow(++rowcount);
+				 int columnCount = 0;
+		                	XSSFCell cell1 = row.createCell(++columnCount);
+		                    cell1.setCellValue(listitem.getName()); 
+		                    XSSFCell cell2 = row.createCell(++columnCount);
+		                    cell2.setCellValue(listitem.getId());
+		                    XSSFCell cell3 = row.createCell(++columnCount);
+		                    cell3.setCellValue(listitem.getRate());
+		                    XSSFCell cell4 = row.createCell(++columnCount);
+		                    cell4.setCellValue(listitem.getComment());
+		                    XSSFCell cell5 = row.createCell(++columnCount);
+		                    cell5.setCellValue(listitem.getRecommend());  
+			}
 			
 			// Type your code here
-			
-		
-			}
 			// Do not modify the lines given below
 			 out = new FileOutputStream(filename);
-			hwb.write(out);
+			xwb.write(out);
 		
-			return hwb;
+			return xwb;
 			}
 		catch (Exception e) {
 				e.printStackTrace();
@@ -44,4 +60,3 @@ public class ExcelGenerator {
 		
 	}
 }
-*/
